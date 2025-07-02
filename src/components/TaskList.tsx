1b92a2cd89a5e844e790711d6232343dabb5b75d
ayan-de <deayan@gmail.com>
2025-07-02 13:23:14 +0530

    Made into a very basic redux app

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,27 +1,42 @@
-import type { TaskData } from "../types";
+// import type { TaskData } from "../types";
 
 import Task from "./Task";
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTaskState, RootState, AppDispatch } from '../lib/store';
 
-type TaskListProps = {
-  /** Checks if it's in loading state */
-  loading?: boolean;
-  /** The list of tasks */
-  tasks: TaskData[];
-  /** Event to change the task to pinned */
-  onPinTask: (id: string) => void;
-  /** Event to change the task to archived */
-  onArchiveTask: (id: string) => void;
-};
 
-export default function TaskList({
-  loading=false,
-  tasks,
-  onPinTask,
-  onArchiveTask,
-}: TaskListProps) {
-    const events = {
-    onPinTask,
-    onArchiveTask,
+// type TaskListProps = {
+//   /** Checks if it's in loading state */
+//   loading?: boolean;
+//   /** The list of tasks */
+//   tasks: TaskData[];
+//   /** Event to change the task to pinned */
+//   onPinTask: (id: string) => void;
+//   /** Event to change the task to archived */
+//   onArchiveTask: (id: string) => void;
+// };
+
+export default function TaskList() {
+  // We're retrieving our state from the store
+  const tasks = useSelector((state: RootState) => {
+    const tasksInOrder = [
+      ...state.taskbox.tasks.filter((t) => t.state === 'TASK_PINNED'),
+      ...state.taskbox.tasks.filter((t) => t.state !== 'TASK_PINNED'),
+    ];
+    const filteredTasks = tasksInOrder.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === 'TASK_PINNED'
+    );
+    return filteredTasks;
+  });
+  const { status } = useSelector((state: RootState) => state.taskbox);
+  const dispatch = useDispatch<AppDispatch>();
+  const pinTask = (value: string) => {
+    // We're dispatching the Pinned event back to our store
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_PINNED' }));
+  };
+  const archiveTask = (value: string) => {
+    // We're dispatching the Archive event back to our store
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_ARCHIVED' }));
   };
  const LoadingRow = (
     <div className="loading-item">
@@ -31,7 +46,7 @@ export default function TaskList({
       </span>
     </div>
   );
-  if (loading) {
+  if (status === "loading") {
     return (
       <div className="list-items" data-testid="loading" key={"loading"}>
         {LoadingRow}
@@ -55,14 +70,19 @@ export default function TaskList({
     );
   }
 
-  const tasksInOrder = [
-    ...tasks.filter((t) => t.state === "TASK_PINNED"),
-    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
-  ];
+//   const tasksInOrder = [
+//     ...tasks.filter((t) => t.state === "TASK_PINNED"),
+//     ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+//   ];
   return (
-    <div className="list-items">
-      {tasksInOrder.map((task) => (
-        <Task key={task.id} task={task} {...events} />
+    <div className="list-items" data-testid="success" key="success">
+    {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onPinTask={pinTask}
+          onArchiveTask={archiveTask}
+        />
       ))}
     </div>
   );
