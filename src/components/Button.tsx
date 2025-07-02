2c9d5ae341c5a4a6d5329b69a9e5440d2c0c1818
ayan-de <deayan@gmail.com>
2025-07-02 18:54:49 +0530

    Add icon support to Button component and update styles for better alignment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import "../stories/button.css";
+import searchIcon from '../images/search.svg';
 
 export interface ButtonProps {
     /** Is this the principal call to action on the page? */
@@ -9,6 +10,8 @@ export interface ButtonProps {
     size?: "small" | "medium" | "large";
     /** Button contents */
     label: string;
+
+    icon?: boolean;
     /** Optional click handler */
     onClick?: () => void;
 }
@@ -17,22 +20,26 @@ export const Button = ({
     primary = true,
     size = "medium",
     backgroundColor,
-    label="Change this text",
+    label = "Change this text",
+    icon = true,
     onClick,
     ...props
 }: ButtonProps) => {
     const mode = primary
         ? "storybook-button--primary"
         : "storybook-button--secondary";
+const classes = ["storybook-button", `storybook-button--${size}`, mode].join(" ");
+
     return (
         <button
             type="button"
-            className={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
+            className={classes}
             style={{ backgroundColor }}
             onClick={onClick}
             {...props}
         >
+            {icon && <img src={searchIcon} alt="" className="storybook-button-icon" />}
             {label}
         </button>
     );
-}
+};
