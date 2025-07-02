0ebd1724cddb8c01b2a8fe05b72b5eb211ab918e
ayan-de <deayan@gmail.com>
2025-07-02 12:11:33 +0530

    Refactor Storybook configuration and add Task component stories

---
+++ src/components/Task.tsx
@@ -0,0 +1,33 @@
+//we render straightforward markup for Task based on the 
+// existing HTML structure of the Todos application
+type TaskData = {
+  id: string;
+  title: string;
+  state: 'TASK_ARCHIVED' | 'TASK_INBOX' | 'TASK_PINNED';
+};
+
+type TaskProps = {
+  task: TaskData;
+  onArchiveTask: (id: string) => void;
+  onPinTask: (id: string) => void;
+};
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}: TaskProps) {
+  return (
+    <div className="list-item">
+      <label htmlFor={`title-${id}`} aria-label={title}>
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          id={`title-${id}`}
+        />
+      </label>
+    </div>
+  );
+}
